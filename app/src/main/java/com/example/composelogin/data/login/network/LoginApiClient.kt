c6f2c467270bb44c099e5c7a20c1f39c5397f382
CristianRamirezC <cristiancrc97@hotmail.com>
2022-10-12 09:34:54 -0500

    applying dependency Injection using Dagger Hilt

--- app/src/main/java/com/example/composelogin/data/login/network/LoginApiClient.kt
+++ app/src/main/java/com/example/composelogin/data/login/network/LoginApiClient.kt
@@ -4,7 +4,7 @@ import com.example.composelogin.data.login.network.response.LoginResponse
 import retrofit2.Response
 import retrofit2.http.GET
 
-interface LoginClient {
+interface LoginApiClient {
     @GET("/v3/f78c9d33-28b1-4f81-9cf1-6d6ff78fa014")
     suspend fun doLogin(): Response<LoginResponse>
 }
--- app/src/main/java/com/example/composelogin/data/login/network/LoginService.kt
+++ app/src/main/java/com/example/composelogin/data/login/network/LoginService.kt
@@ -3,15 +3,14 @@ package com.example.composelogin.data.login.network
 import com.example.composelogin.core.network.RetrofitHelper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import retrofit2.create
 import javax.inject.Inject
 
-class LoginService @Inject constructor() {
-    private val retrofit = RetrofitHelper.getRetrofit()
-
+class LoginService @Inject constructor(
+    private val api: LoginApiClient
+) {
     suspend fun doLogin(user: String, password: String): Boolean {
         return withContext(Dispatchers.IO) {
-            val response = retrofit.create(LoginClient::class.java).doLogin()
+            val response = api.doLogin()
             response.body()?.success ?: false
         }
     }
